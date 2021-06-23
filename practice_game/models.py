65279e80bac0fbed2ee740b17e1073f31fb84666
biubiuworld <77176093+biubiuworld@users.noreply.github.com>
2021-06-23 15:46:27 -0700

    main design

--- practice_game/models.py
+++ practice_game/models.py
@@ -21,6 +21,9 @@ class Constants(BaseConstants):
     name_in_url = 'practice_game'
     players_per_group = None
     num_rounds = 2
+    V = 0.05
+    f = 0.5
+    h = 10
 
 
 class Subsession(BaseSubsession):
@@ -36,25 +39,60 @@ class Subsession(BaseSubsession):
 
     def generate_observed_players_eachround(self):
         for p in self.get_players():
-            others_last_opinions = []
+            p.participant.vars['others_last_opinions'] = []
+            p.participant.vars['others_id_in_group'] = []
             for other in p.get_others_in_subsession():
-                others_last_opinions.append(other.in_round(p.round_number).opinion_last_round)
-            observed_players_this_round = random.sample(others_last_opinions, 2)
-            p.observed_player1 = observed_players_this_round[0]
-            p.observed_player2 = observed_players_this_round[1]
+                p.participant.vars['others_id_in_group'].append(other.id_in_group) #stores all 10 other players' ids
+                p.participant.vars['others_last_opinions'].append(other.in_round(p.round_number).opinion_last_round) #stores all 10 other players' last round opinions
+            observed_players_id_this_round = random.sample(p.participant.vars['others_id_in_group'], 2) #randomly choose 2 observed players (id)
+            p.observed_id_player1 = observed_players_id_this_round[0]
+            p.observed_id_player2 = observed_players_id_this_round[1]
+            p.observed_opinion_player1 = p.participant.vars['others_last_opinions'][p.participant.vars['others_id_in_group'].index(p.observed_id_player1)]
+            p.observed_opinion_player2 = p.participant.vars['others_last_opinions'][p.participant.vars['others_id_in_group'].index(p.observed_id_player2)]
 
+            if p.observed_id_player1 in p.participant.vars['neighbors_id_set']:
+                p.disconnect_with_player1 = 1
+            else:
+                p.disconnect_with_player1 = 0
+
+            if p.observed_id_player2 in p.participant.vars['neighbors_id_set']:
+                p.disconnect_with_player2 = 1
+            else:
+                p.disconnect_with_player2 = 0
+
+
+    # def collect_neighbors(self):
+    #     for p in self.get_players():
+    #         if p.if_connect_player1 == 1:
+    #             p.participant.vars['neighbors_id_set'].append(p.observed_id_player1)
+    #         if p.if_connect_player2 == 1:
+    #             p.participant.vars['neighbors_id_set'].append(p.observed_id_player2)
+    #         p.participant.vars['neighbors_id_set'] = list(dict.fromkeys(p.participant.vars['neighbors_id_set']))
 
 class Group(BaseGroup):
     pass
 
 
 class Player(BasePlayer):
-    initial_opinion = models.FloatField(min=0, max=1)
+    initial_opinion = models.FloatField(min=0, max=1) # The initial opinion assigned to each player
     opinion_last_round = models.FloatField(min=0, max=1)
     opinion_this_round = models.FloatField(min=0, max=1, label='Please update your opinion in this round')
-    observed_player1 = models.FloatField(min=0, max=1)
-    observed_player2 = models.FloatField(min=0, max=1)
+    observed_opinion_player1 = models.FloatField(min=0, max=1) # The observed player 1's last round opinion
+    observed_opinion_player2 = models.FloatField(min=0, max=1) # The observed player 2's last round opinion
+    observed_id_player1 = models.IntegerField(min=1, max=11) # The observed player 1's ID
+    observed_id_player2 = models.IntegerField(min=1, max=11) # The observed player 2's ID
+    if_connect_player1 = models.BooleanField(label='Do you want to connect with Player 1?', blank=True) # Whether to connect with player 1
+    if_connect_player2 = models.BooleanField(label='Do you want to connect with Player 2?', blank=True) # Whether to connect with player 2
+    num_neighbors = models.IntegerField(initial=0)
+    disconnect_with_player1 = models.BooleanField()
+    disconnect_with_player2 = models.BooleanField()
 
-
+    # def live_getselectedneighbor(self, data):
+    #     print(data)
+    #     if "player1" in data:
+    #         print('connected with player 1')
+    #     if "player2" in data:
+    #         print('connected with player 2')
+    #     print('end of live data send')
 
 
--- practice_game/pages.py
+++ practice_game/pages.py
@@ -5,7 +5,8 @@ import random
 class PracticeGameInstruction(Page):
     def is_displayed(self):
         return self.round_number == 1
-
+    def before_next_page(self):
+        self.participant.vars['neighbors_id_set'] = []
 
 
 class GenerateInitialOpinionWaitPage(WaitPage):
@@ -25,20 +26,54 @@ class GenerateObservedPlayersWaitPage(WaitPage):
     after_all_players_arrive = 'generate_observed_players_eachround'
 
 
-class OpinionUpdate(Page):
+class NeighborUpdate(Page):
     form_model = 'player'
-    form_fields = ['opinion_this_round']
+    form_fields = ['if_connect_player1', 'if_connect_player2']
+    # live_method = 'live_getselectedneighbor'
+
 
     def vars_for_template(self):
         return {
             'opinion_last_round': self.player.opinion_last_round,
-            'observed_player1': self.player.observed_player1,
-            'observed_player2': self.player.observed_player2,
-            'last_round': self.round_number - 1
+            'observed_player1': self.player.observed_opinion_player1,
+            'observed_player2': self.player.observed_opinion_player2,
+            'last_round': self.round_number - 1,
+            'if_connect_player1': self.player.if_connect_player1,
+            'if_connect_player2': self.player.if_connect_player2,
         }
 
+    def before_next_page(self):
+        if (self.player.if_connect_player1 == 1) & (self.player.disconnect_with_player1 == 0):
+            self.participant.vars['neighbors_id_set'].append(self.player.observed_id_player1)
 
+        if (self.player.if_connect_player1 == 0) & (self.player.disconnect_with_player1 == 1):
+            self.participant.vars['neighbors_id_set'].remove(self.player.observed_id_player1)
 
+        if (self.player.if_connect_player2 == 1) & (self.player.disconnect_with_player2 == 0):
+            self.participant.vars['neighbors_id_set'].append(self.player.observed_id_player2)
+
+        if (self.player.if_connect_player2 == 0) & (self.player.disconnect_with_player2 == 1):
+            self.participant.vars['neighbors_id_set'].remove(self.player.observed_id_player2)
+
+        # self.participant.vars['neighbors_id_set'] = list(dict.fromkeys(self.participant.vars['neighbors_id_set']))
+        self.participant.vars['neighbors_opinion_set'] = []
+        for neighbor in self.participant.vars['neighbors_id_set']:
+            self.participant.vars['neighbors_opinion_set'].append(self.participant.vars['others_last_opinions'][self.participant.vars['others_id_in_group'].index(neighbor)])
+        self.player.num_neighbors = len(self.participant.vars['neighbors_opinion_set'])
+        print(f'I am player {self.player.id_in_group}, my neighbors and their last round opinions are')
+        print(self.participant.vars['neighbors_id_set'])
+        print(self.participant.vars['neighbors_opinion_set'])
+
+class OpinionUpdate(Page):
+    form_model = 'player'
+    form_fields = ['opinion_this_round']
+    def vars_for_template(self):
+        return {
+            'opinion_last_round': self.player.opinion_last_round,
+            'last_round': self.round_number - 1,
+            'num_neighbors': self.player.num_neighbors,
+            'neighbors_opinion_set': self.participant.vars['neighbors_opinion_set'],
+        }
 
 class Results(Page):
     def is_displayed(self):
@@ -48,4 +83,4 @@ class Results(Page):
             'player_all_rounds': self.player.in_all_rounds(),
         }
 
-page_sequence = [PracticeGameInstruction,GenerateInitialOpinionWaitPage, OpinionUpdateWaitPage, GenerateObservedPlayersWaitPage, OpinionUpdate, Results]
+page_sequence = [PracticeGameInstruction,GenerateInitialOpinionWaitPage, OpinionUpdateWaitPage, GenerateObservedPlayersWaitPage, NeighborUpdate, OpinionUpdate, Results]
