ee332801337625be1f4b0dbf6a62881ba67a2666
biubiuworld <77176093+biubiuworld@users.noreply.github.com>
2021-07-19 23:11:02 -0700

    pilot

--- game_VhFmH0/models.py
+++ game_VhFmH0/models.py
@@ -21,7 +21,7 @@ Your app description
 class Constants(BaseConstants):
     name_in_url = 'game_VhFmH0'
     players_per_group = None
-    num_rounds = 2
+    num_rounds = 30
     min_opinion = 0
     V = 0.025
     f = 0.5
