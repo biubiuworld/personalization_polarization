af04b4a8003cb5aedc5d623dc58fd0e893a0f8eb
biubiuworld <77176093+biubiuworld@users.noreply.github.com>
2021-08-01 16:00:03 -0700

    test pilot

--- game_VhFhH0/models.py
+++ game_VhFhH0/models.py
@@ -21,7 +21,7 @@ Your app description
 class Constants(BaseConstants):
     name_in_url = 'game_VhFhH0'
     players_per_group = None
-    num_rounds = 10
+    num_rounds = 5
     min_opinion = 0.
     V = 250
     f = 0.9
--- settings.py
+++ settings.py
@@ -1,12 +1,12 @@
 from os import environ
 
 SESSION_CONFIGS = [
-    dict(
-       name='practice_game',
-       display_name="Practice Game",
-       num_demo_participants=3,
-       app_sequence=['practice_game']
-    ),
+    # dict(
+    #    name='practice_game',
+    #    display_name="Practice Game",
+    #    num_demo_participants=3,
+    #    app_sequence=['practice_game']
+    # ),
     # dict(
     #     name='game_VhFlH0',
     #     display_name="game_VhFlH0",
@@ -43,18 +43,18 @@ SESSION_CONFIGS = [
     #     num_demo_participants=3,
     #     app_sequence=['game_VhFhH10']
     # ),
-    # dict(
-    #     name='quiz',
-    #     display_name="pilot",
-    #     num_demo_participants=1,
-    #     app_sequence=['quiz', 'practice_game', 'game_VlFmH0', 'game_VhFhH0']
-    # ),
     dict(
         name='quiz',
         display_name="pilot",
         num_demo_participants=1,
-        app_sequence=['quiz']
+        app_sequence=['quiz', 'practice_game', 'game_VlFmH0', 'game_VhFhH0']
     ),
+    # dict(
+    #     name='quiz',
+    #     display_name="pilot",
+    #     num_demo_participants=1,
+    #     app_sequence=['quiz']
+    # ),
     # dict(
     #     name='Experiment_Survey',
     #     display_name="Experiment_Survey",
