bcb990316bc99007a5498a32b208aa8c20fb7600
biubiuworld <77176093+biubiuworld@users.noreply.github.com>
2022-04-25 14:12:09 -0700

    vlh10

--- settings.py
+++ settings.py
@@ -2,10 +2,10 @@ from os import environ
 
 SESSION_CONFIGS = [
     # dict(
-    #    name='Vlh10_game',
-    #    display_name="Vlh10_game",
+    #    name='Vlh0_game',
+    #    display_name="Vlh0_game",
     #    num_demo_participants=9,
-    #    app_sequence=['Vlh10_game']
+    #    app_sequence=['Vlh0_game']
     # ),   
     # dict(
     #    name='Vlh0_practice',
@@ -13,18 +13,18 @@ SESSION_CONFIGS = [
     #    num_demo_participants=9,
     #    app_sequence=['Vlh0_practice']
     # ),  
-    # dict(
-    #     name='Treatment4',
-    #     display_name="Treatment4",
-    #     num_demo_participants=9,
-    #     app_sequence=['quiz','Vlh10_practice', 'Vlh10_game']
-    # ),
     dict(
-        name='Treatment3',
-        display_name="Treatment3",
+        name='Treatment4',
+        display_name="Treatment4",
         num_demo_participants=9,
-        app_sequence=['quiz','Vlh0_practice', 'Vlh0_game']
+        app_sequence=['quiz','Vlh10_practice', 'Vlh10_game']
     ),
+    # dict(
+    #     name='Treatment3',
+    #     display_name="Treatment3",
+    #     num_demo_participants=9,
+    #     app_sequence=['quiz','Vlh0_practice', 'Vlh0_game']
+    # ),
     # dict(
     #     name='Treatment2',
     #     display_name="Treatment2",
