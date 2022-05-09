0abcdfd5a494f12bdaafade13828d4ecdc2d99e1
biubiuworld <77176093+biubiuworld@users.noreply.github.com>
2022-05-09 10:58:59 -0700

    update

--- fHh10_game/pages.py
+++ fHh10_game/pages.py
@@ -36,7 +36,7 @@ class NeighborUpdate(Page):
                    'disconnect_with_neighbor_1', 'disconnect_with_neighbor_2', 'disconnect_with_neighbor_3', 'disconnect_with_neighbor_4', 'disconnect_with_neighbor_5',
                    'disconnect_with_neighbor_6', 'disconnect_with_neighbor_7', 'disconnect_with_neighbor_8']
 
-    timeout_seconds = 90
+    # timeout_seconds = 90
 
     def vars_for_template(self):
         self.participant.vars['neighbors_opinion_set'] = []
--- settings.py
+++ settings.py
@@ -1,15 +1,15 @@
 from os import environ
 
 SESSION_CONFIGS = [
-    # dict(
-    #    name='fHh10_game',
-    #    display_name="fHh10_game",
-    #    num_demo_participants=9,
-    #    app_sequence=['fHh10_game']
-    # ),   
+    dict(
+       name='fHh10_game',
+       display_name="fHh10_game",
+       num_demo_participants=9,
+       app_sequence=['fHh10_game']
+    ),   
     # dict(
     #    name='fHh10_practice',
-    #    display_name="fHh0_practice",
+    #    display_name="fHh10_practice",
     #    num_demo_participants=9,
     #    app_sequence=['fHh10_practice']
     # ),  
@@ -25,12 +25,12 @@ SESSION_CONFIGS = [
     #     num_demo_participants=9,
     #     app_sequence=['quiz','Vlh0_practice', 'Vlh0_game']
     # ),
-    dict(
-        name='Treatment2',
-        display_name="Treatment2",
-        num_demo_participants=9,
-        app_sequence=['quiz_VhFh','fHh10_practice', 'fHh10_game']
-    ),
+    # dict(
+    #     name='Treatment2',
+    #     display_name="Treatment2",
+    #     num_demo_participants=9,
+    #     app_sequence=['quiz_VhFh','fHh10_practice', 'fHh10_game']
+    # ),
     # dict(
     #     name='Treatment1',
     #     display_name="Treatment1",
